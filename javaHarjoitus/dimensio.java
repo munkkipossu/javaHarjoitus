c333d439e001919e2cadc9f50d7a14a4f4d3d6b2
mp <mp@82.130.19.212>
2013-03-19 19:50:59 +0200

    testi, koitetaan commitoida kappale.java netbeansin kautta

---
+++ javaHarjoitus/dimensio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mp
+ */
+public interface dimensio {
+    double sade;
+    
+    void tormays(kappale 1, kappale 2);
+    
+}
