adeebe8cb1072fe6349e08400ff67657da161bf6
mp <mp@82.130.19.212>
2013-03-19 19:42:07 +0200

    testi, koitetaan commitoida kappale.java netbeansin kautta

---
+++ javaHarjoitus/kappale.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mp
+ */
+public class kappale implements dimensio{
+    private double massa;
+    private double[] paikka;
+    private double[] nopeus;
+    
+}
