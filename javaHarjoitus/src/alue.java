9959ffeee021267628bb86453c8df0c22db4dce7
Teemu O Laakso <tolaakso@dhp-11.cs.helsinki.fi>
2013-03-25 17:25:36 +0200

    Perus säätöä ja ohjelmoinnin uudelleen opettelua. Hidasta touhua.

---
+++ javaHarjoitus/src/alue.java
@@ -0,0 +1,20 @@
+
+/*
+ * Tämä luokka sisältää tilassa ilmenevät kappaaleet (pallot ja sun muut), 
+ * sekä määrittelee kappaleille saalitun alueen (reunojen avulla). 
+ */
+public class alue {
+    
+    // Kun alueen leveys ja korkes on määritelty, voidaan niiden avulla luoda 
+    // sitä määrittävät reunat (suorakaideoliot). 
+    private double leveys;
+    private double korkeus;
+    
+    // suorakaiteet jossain titotomuodossa
+    private suorakaide reunat;
+    
+    // Alueella oleskelevat ja liikkuvat kappaleet
+    private kappale taulukkoKappaleista;
+    
+
+}
---
+++ javaHarjoitus/src/tila.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tolaakso
+ */
+public class tila {
+    private double aikaAskel;
+    private double maxAika;
+    private alue kentta;
+    
+    
+    public tila(){
+        this.aikaAskel = 0.01;
+        this.maxAika = 0.0;
+        this.kentta = new alue();
+    }    
+}
