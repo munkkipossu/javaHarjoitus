cb99027a5d59e15afa04ea2026ab90f2eea61d96
Teemu O Laakso <tolaakso@dhp-11.cs.helsinki.fi>
2013-03-25 16:33:19 +0200

    lisää sekoilua

---
+++ javaHarjoitus/src/physics.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tolaakso
+ */
+public interface physics {
+    public double sade;
+    void tormays(Object eka, Object toka);
+  
+}
